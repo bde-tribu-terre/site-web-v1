4a33641f9c185797e01d6fc592592cb5f58c33d8
Anaël Laptop <anael@barodine.fr>
2020-02-05 17:56:24 +0100

    Squelette finalisé.

--- admin/controleur/controleur.php
+++ admin/controleur/controleur.php
@@ -10,6 +10,36 @@ function CtlConnexion($messageRetour) {
     afficherConnexion($messageRetour);
 }
 
+function CtlVerifConnexion($login, $mdp) {
+    if ($login == 'login' && $mdp == 'mdp') {
+        CtlMenu('');
+    } else {
+        CtlConnexionErreur('Erreur : Mot de passe invalide.');
+    }
+    /*
+    if (!empty($login) && !empty($mdp)) {
+        $categorie = checkUser($login, $mdp);
+        if ($categorie != false) {
+            switch ($categorie) {
+                case 'Médecin':
+                    CtlMedecin('');
+                    break;
+                case 'Agent':
+                    CtlAgent('');
+                    break;
+                case 'Directeur':
+                    CtlDirecteur('');
+                    break;
+            }
+        } else {
+            throw new Exception("<p>Connexion :<br><b>Login ou mot de passe invalide.</b></p>");
+        }
+    } else {
+        throw new Exception("<p>Connexion :<br><b>Veuillez remplir tous les champs.</b></p>");
+    }
+    */
+}
+
 function CtlConnexionErreur($messageErreur) {
     afficherConnexion($messageErreur);
 }
@@ -17,7 +47,7 @@ function CtlConnexionErreur($messageErreur) {
 ########################################################################################################################
 # Gabarit Menu                                                                                                         #
 ########################################################################################################################
-function CltMenu($messageRetour) {
+function CtlMenu($messageRetour) {
     afficherMenu($messageRetour);
 }
 
