d60f6492e67138a4c642f2365dba3ba1efe42475
Anaël Laptop <anael@barodine.fr>
2020-02-05 17:51:05 +0100

    Création admin.php.

---
+++ admin/admin.php
@@ -0,0 +1,14 @@
+<?php // TODO : Sessions.
+require_once('./controleur/controleur.php');
+try {
+    if (isset($_POST['formConnexion_seConnecter'])) {
+        CtlVerifConnexion(
+            $_POST['formConnexion_login'],
+            $_POST['formConnexion_mdp']
+        );
+    } else { // On vient d'arriver sur la page sans n'avoir rien cliqué.
+        CtlConnexion();
+    }
+} catch (Exception $e) {
+    CtlErreur($e->getMessage());
+}
