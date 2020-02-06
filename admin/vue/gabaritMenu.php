e9bdcb795ff2d38c73ac2d4286e128d8ddda25d6
Anaël Laptop <anael@barodine.fr>
2020-02-06 19:33:26 +0100

    Changement de l'emplacement message retour & ID fieldsets.

--- admin/vue/gabaritMenu.php
+++ admin/vue/gabaritMenu.php
@@ -5,14 +5,34 @@
     <meta charset="UTF-8">
 </head>
 <body>
-<?php
-if (!empty($messageRetour)) { // Si il y a un message de retour, c'est à dire un message après avoir bien ou mal envoyé un formulaire, il s'affiche ici.
-    echo '<fieldset id="message_fieldset">' . '<legend>Message</legend>' . '<p>' . $messageRetour . '</p>' . '</fieldset>';
-}
-?>
 <fieldset>
     <h1>MENU ADMINISTRATEUR</h1>
     <h2>Bienvenue <?php echo $nomMembre ?> 😊</h2>
+    <?php
+    if (!empty($messageRetour)) { // Si il y a un message de retour, c'est à dire un message après avoir bien ou mal envoyé un formulaire, il s'affiche ici.
+        echo '<fieldset id="message_fieldset">' . '<legend>Message</legend>' . '<p>' . $messageRetour . '</p>' . '</fieldset>';
+    }
+    ?>
+    <fieldset id="articles_fieldset">
+        <h3>Articles</h3>
+        <form id="formArticles" action="admin.php" method="post">
+            <input type="submit" value="Créer un article" name="formArticles_creerArticle">
+            <input type="submit" value="Modifier un article" name="formArticles_modifierArticle">
+        </form>
+    </fieldset>
+    <fieldset id="events_fieldset">
+        <h3>Events</h3>
+        <form id="formEvents" action="admin.php" method="post">
+            <input type="submit" value="Créer un évent" name="formEvents_creerEvent">
+            <input type="submit" value="Modifier un évent" name="formEvents_modifierEvent">
+        </form>
+    </fieldset>
+    <fieldset id="monCompte_fieldset">
+        <h3>Mon compte</h3>
+        <form id="formMonCompte" action="admin.php" method="post">
+            <input type="submit" value="Paramètres de mon compte" name="formMonCompte_parametres">
+        </form>
+    </fieldset>
 </fieldset>
 </body>
 </html>
