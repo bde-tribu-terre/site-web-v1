1a453e92aff12cecde9b2918882e59af013f4df5
Anaël Laptop <anael@barodine.fr>
2020-02-06 19:45:09 +0100

    Ajout '!'.

--- admin/vue/gabaritMenu.php
+++ admin/vue/gabaritMenu.php
@@ -7,7 +7,7 @@
 <body>
 <fieldset>
     <h1>MENU ADMINISTRATEUR</h1>
-    <h2>Bienvenue <?php echo $nomMembre ?> 😊</h2>
+    <h2>Bienvenue <?php echo $nomMembre ?> ! 😊</h2>
     <?php
     if (!empty($messageRetour)) { // Si il y a un message de retour, c'est à dire un message après avoir bien ou mal envoyé un formulaire, il s'affiche ici.
         echo '<fieldset id="message_fieldset">' . '<legend>Message</legend>' . '<p>' . $messageRetour . '</p>' . '</fieldset>';
