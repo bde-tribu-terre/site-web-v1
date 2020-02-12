3b4f6d86a70f83ba54cfe6d60c3c82bd9a56f561
Anaël Laptop <anael@barodine.fr>
2020-02-12 20:21:51 +0100

    Ajout du lien vers la page nous contacter.

--- global/footer.php
+++ global/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <p>
         Vous avez des questions sur Tribu-Terre ou les derniers évènements ?<br/>
-        <a href="NousContacter.php">Contactez nous !</a>
+        <a href="<?php echo $prefixe . '../accueil/nous-contacter.php' ?>">Contactez nous !</a>
     </p>
 </footer>
