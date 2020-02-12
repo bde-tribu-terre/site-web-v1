0dae9dac0a058dcf0531e1f4f319f97045b47c1c
Anaël Laptop <anael@barodine.fr>
2020-02-12 22:04:17 +0100

    Correction du lien contactez-nous.

--- global/footer.php
+++ global/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <p>
         Vous avez des questions sur Tribu-Terre ou les derniers évènements ?<br/>
-        <a href="<?php echo $prefixe . '../nous-contacter.php' ?>">Contactez nous !</a>
+        <a href="<?php echo $prefixe . 'nous-contacter.php' ?>">Contactez nous !</a>
     </p>
 </footer>
