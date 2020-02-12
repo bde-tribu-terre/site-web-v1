a0c98e2903dab6300342b2734a73117b28981de1
Anaël Laptop <anael@barodine.fr>
2020-02-12 22:10:12 +0100

    Tests MEF & reduc taille texte (h1 pour titres).

--- vue/gabarits/gabaritAccueil.php
+++ vue/gabarits/gabaritAccueil.php
@@ -1,9 +1,18 @@
-<p>
+<h1>
     Accueil
-</p>
+</h1>
 <p>
     Events : <?php echo $events ?>
 </p>
 <p>
     Articles : <?php echo $articles ?>
 </p>
+<div id="divEssaisMEF">
+    <h1>h1</h1>
+    <h2>h2</h2>
+    <h3>h3</h3>
+    <h4>h4</h4>
+    <h5>h5</h5>
+    <h6>h6</h6>
+    <p>p</p>
+</div>
