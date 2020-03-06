fbf30be97e0278c77c7c5d760bcfdd9d209980bc
Anaël Laptop <anael@barodine.fr>
2020-03-06 18:16:03 +0100

    Ajout Ajouter Journal.

--- admin/admin.php
+++ admin/admin.php
@@ -9,13 +9,26 @@ try {
             $_POST['formConnexion_mdp']
         );
     }
+    // Gabarit Journal
+    elseif (isset($_POST['formAjouterJournal_ajouterJournal'])) {
+        CtlAjouterJournal(
+            $_POST['formAjouterJournal_titreJournal'],
+            $_POST['formAjouterJournal_moisJournal'],
+            $_POST['formAjouterJournal_anneeJournal'],
+            'formAjouterJournal_fichierPDF'
+        );
+    }
     // Gabarit Menu
+    elseif (isset($_POST['formJournal_gererJournal'])) {
+        CtlJournal('');
+    }
     elseif (isset($_POST['formMonCompte_parametres'])) {
         CtlParametresCompte('');
     } elseif (isset($_POST['formDeconnexion_deconnexion'])) {
         CtlDeconnexion('');
     }
     // Gabarit Paramètres Compte
+    // Globaux : apparaissent dans plusieurs gabarits
     elseif (isset($_POST['formRetourMenu_retourMenu'])) {
         CtlMenu('');
     }
