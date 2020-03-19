950a6b5472bef89012448a742cbd1e4b28be843f
Anaël iMac <anael@barodine.fr>
2020-03-19 23:40:36 +0100

    Et il manquait encore à changer l'ID ici.

--- mvcpublic/vue/gabarits/gabaritAccueil.php
+++ mvcpublic/vue/gabarits/gabaritAccueil.php
@@ -15,11 +15,11 @@
                 </div>
 
                 <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <a class="left carousel-control" href="#carouselGoodies" role="button" data-slide="prev">
                     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                 </a>
-                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <a class="right carousel-control" href="#carouselGoodies" role="button" data-slide="next">
                     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
                 </a>
