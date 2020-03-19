f8d17a3c8b0e0dec2df8b677a34bf52e107deb9e
Anaël iMac <anael@barodine.fr>
2020-03-19 20:34:22 +0100

    Affichage correct des mois.

--- mvcpublic/vue/vue.php
+++ mvcpublic/vue/vue.php
@@ -52,6 +52,21 @@ function afficherJournaux($prefixe) {
     $journaux = scandir($prefixe . 'ressources/journaux');
     natsort($journaux);
     array_reverse($journaux);
+
+    $arrayMois = [
+        '01' => 'Janvier',
+        '02' => 'Février',
+        '03' => 'Mars',
+        '04' => 'Avril',
+        '05' => 'Mai',
+        '06' => 'Juin',
+        '07' => 'Juillet',
+        '08' => 'Août',
+        '09' => 'Septembre',
+        '10' => 'Octobre',
+        '11' => 'Novembre',
+        '12' => 'Décembre'
+    ];
     foreach ($journaux as $repertoire) {
         if (file_exists($prefixe . 'ressources/journaux/' . $repertoire . '/desc.txt') && $repertoire != '.' && $repertoire != '..') {
             $desc = file($prefixe . 'ressources/journaux/' . $repertoire . '/desc.txt');
@@ -63,7 +78,7 @@ function afficherJournaux($prefixe) {
                 '<div class="col-sm-3">' .
                     '<div class="well">' .
                         '<h3>' . $titre . '</h3>' .
-                        '<h4>' . $dateParution . '</h4>' .
+                        '<h4>' . $arrayMois[substr($dateParution, -2)] . ' ' . substr($dateParution, 0, -2) . '</h4>' .
                         '<a href="' . $lienJournal . '">' .
                             '<h4>Lire en ligne</h4>' .
                         '</a>' .
