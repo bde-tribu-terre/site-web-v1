0e7971ef1ba3b4b23c1a46b5207ad4b261132aee
Anaël iMac <anael@barodine.fr>
2020-03-19 23:04:19 +0100

    Affichage du carousel des goodies.

--- mvcpublic/vue/vue.php
+++ mvcpublic/vue/vue.php
@@ -6,6 +6,52 @@ function afficherAccueil($prefixe) {
     $title = 'Accueil';
     $gabarit = $prefixe . 'mvcpublic/vue/gabarits/gabaritAccueil.php';
 
+    # Goodies
+    $goodiesIndicators = '';
+    $goodies ='';
+    $listeGoodies = scandir($prefixe . 'ressources/goodies');
+    natsort($listeGoodies);
+
+    foreach ($listeGoodies as $repertoire) {
+        if (
+            file_exists($prefixe . 'ressources/journaux/' . $repertoire . '/desc.txt') &&
+            file_exists($prefixe . 'ressources/journaux/' . $repertoire . '/attr.txt') &&
+            file_exists($prefixe . 'ressources/journaux/' . $repertoire . '/img.png') &&
+            $repertoire != '.' &&
+            $repertoire != '..'
+        ) {
+            $attr = file($prefixe . 'ressources/goodies/' . $repertoire . '/attr.txt');
+            $nomGoodie = $attr[0];
+            $prixAdherent = $attr[1];
+            $prixNonAdherent = $attr[2];
+            $etat = $attr[3]; // 0 : Caché, 1 : Disponible, 2 : Bientôt disponible, 3 : En rupture de stock
+            if ($etat != 1) {
+                continue;
+            }
+            $lienImg = $prefixe . 'ressources/goodies/' . $repertoire . '/img.png';
+            $goodiesIndicators .= '<li data-target="carouselGoodies" data-slide-to="' . $repertoire . '"';
+            if ($listeGoodies[0] == $repertoire) {
+                $goodiesIndicators .= ' class="active"';
+            }
+            $goodiesIndicators .= '></li>';
+            $goodies .= '<div class="item';
+            if ($listeGoodies[0] == $repertoire) {
+                $goodies .= ' active';
+            }
+            $goodies .=
+                '">' .
+                    '<img src="' . $lienImg . '" alt="Image">' .
+                    '<div class="carousel-caption">' .
+                        '<h3>' . $nomGoodie . '</h3>' .
+                        '<p>' . $prixAdherent . '€ Adhérent | ' . $prixNonAdherent . '€ Non-adhérent</p>' .
+                    '</div>' .
+                '</div>';
+        }
+    }
+
+    # Events
+    # pass
+
     require_once($prefixe . 'mvcpublic/vue/cadre.php');
 }
 
