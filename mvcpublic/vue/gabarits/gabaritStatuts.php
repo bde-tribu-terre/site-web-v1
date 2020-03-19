14b155ccbde987bc5b3898c10a502507b6890dcb
Anaël Laptop <anael@barodine.fr>
2020-03-19 15:11:37 +0100

    Affichage des statuts.

--- mvcpublic/vue/gabarits/gabaritStatuts.php
+++ mvcpublic/vue/gabarits/gabaritStatuts.php
@@ -1,4 +1,15 @@
 <h1>Statuts</h1>
 <p>
-    Statuts
+    Les statuts de l'association sont, en quelque sorte, le règlement de conduite que doit suivre l'association et ses
+    membres.<br>
+    Il s'agit d'un ensemble d'articles à valeur légale qui détaillent comment doit s'organiser la direction de
+    Tribu-Terre par ses membres actifs.<br>
+    Ils sont consultables à tout moment par n'importe qui. Ils ne peuvent cependant être modifiés que lors d'une
+    Assemblée Générale Extraordinaire (ce qui est détaillé dans les statuts eux-mêmes).
 </p>
+<h3>
+    Les statuts actuels de l'association, modifiés pour la dernière fois le 12 Mars 2019, sont disponibles ici :
+</h3>
+<h3>
+    <a href="<?php echo $prefixe . 'ressources/statuts/2019-03-12-Statuts.pdf' ?>">Consulter les statuts.</a>
+</h3>
