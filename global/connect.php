4bd469e93aef40fc34339420ff3ea8930ba561e8
Anaël iMac <anael@barodine.fr>
2020-03-30 18:49:38 +0200

    Déplacement de getConnect dans connect.php, & SET NAMES UTF8MB4.

--- global/connect.php
+++ global/connect.php
@@ -3,3 +3,10 @@ define("SERVEUR", "localhost");
 define("USER", "root");
 define("PASSWORD", "");
 define("BDD", "tribu-terre");
+
+function getConnect() {
+    $connexion = new PDO('mysql:host='.SERVEUR.';dbname='.BDD,USER,PASSWORD);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexion->query('SET NAMES UTF8MB4'); // UTF8mb4 : Pour pouvoir encoder des émojis
+    return $connexion;
+}
