598c5b9eabb9da4933b4c3d469b76bd9de97eeb0
Anaël iMac <anael@barodine.fr>
2020-03-30 16:16:31 +0200

    Footer fixé au bas réglé.

--- global/footer.php
+++ global/footer.php
@@ -1,12 +1,14 @@
-<footer class="container-fluid text-center fixed-bottom">
-    <p><strong>
-            Vous avez des questions sur Tribu-Terre ou les derniers évènements ?<br/>
-            <a href="<?php echo $prefixe . 'nous-contacter.php' ?>">Contactez nous !</a>
-        </strong></p>
-    <hr>
-    <div class="texteFooter">
-        <p>Tribu-Terre est une association étudiante apolitique à but non lucratif régie par la loi du 1er Juillet 1901.</p>
-        <p><a href="<?php echo $prefixe . 'statuts.php' ?>">Consulter les statuts de l'association.</a></p>
-        <p>Tribu-Terre 2020 | 1A Rue de la Férollerie, 45071, Orléans Cedex 2</p>
+<footer class="page-footer fixed-bottom">
+    <div class="container-fluid text-center">
+        <p><strong>
+                Vous avez des questions sur Tribu-Terre ou les derniers évènements ?<br/>
+                <a href="<?php echo $prefixe . 'nous-contacter.php' ?>">Contactez nous !</a>
+            </strong></p>
+        <hr>
+        <div class="texteFooter">
+            <p>Tribu-Terre est une association étudiante apolitique à but non lucratif régie par la loi du 1er Juillet 1901.</p>
+            <p><a href="<?php echo $prefixe . 'statuts.php' ?>">Consulter les statuts de l'association.</a></p>
+            <p>Tribu-Terre 2020 | 1A Rue de la Férollerie, 45071, Orléans Cedex 2</p>
+        </div>
     </div>
 </footer>
