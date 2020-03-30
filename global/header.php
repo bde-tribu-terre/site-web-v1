fe618718237e9fc59378199c49e6040e63ff3065
Anaël iMac <anael@barodine.fr>
2020-03-30 18:50:23 +0200

    Renommé accueil.php en index.php & liens correspondants.

--- global/header.php
+++ global/header.php
@@ -1,6 +1,6 @@
 <div class="jumbotron">
     <div class="container text-center">
-        <a href="<?php echo $prefixe . 'accueil.php' ?>">
+        <a href="<?php echo $prefixe ?>">
             <img src="<?php echo $prefixe . 'global/images/imgLogo.png' ?>" alt="Logo" height="300">
         </a>
         <p>Association des étudiants en Science de l'Université d'Orléans</p>
@@ -18,7 +18,7 @@
         </div>
         <div class="collapse navbar-collapse" id="navigationHeader"> <!-- Et ça c'est quand l'écran est assez grand -->
             <ul class="nav navbar-nav">
-                <li<?php if ($gabarit == $prefixe . 'mvcpublic/vue/gabarits/gabaritAccueil.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe . 'accueil.php' ?>">Accueil</a></li>
+                <li<?php if ($gabarit == $prefixe . 'mvcpublic/vue/gabarits/gabaritAccueil.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe ?>">Accueil</a></li>
                 <li<?php if ($gabarit == $prefixe . 'mvcpublic/vue/gabarits/gabaritQuiSommesNous.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe . 'qui-sommes-nous.php' ?>">Qui sommes-nous ?</a></li>
                 <li<?php if ($gabarit == $prefixe . 'mvcpublic/vue/gabarits/gabaritEvents.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe . 'events.php' ?>">Évents</a></li>
                 <li<?php if ($gabarit == $prefixe . 'mvcpublic/vue/gabarits/gabaritGoodies.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe . 'goodies.php' ?>">Goodies</a></li>
