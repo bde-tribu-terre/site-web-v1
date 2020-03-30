125a93df2bd82f5bf3dc163df1f8575bf398e79b
Anaël iMac <anael@barodine.fr>
2020-03-30 22:54:47 +0200

    Correction des parternaires

--- mvcpublic/vue/gabarits/gabaritAccueil.php
+++ mvcpublic/vue/gabarits/gabaritAccueil.php
@@ -87,42 +87,52 @@
 </div>
 
 <div class="container text-center">
-    <h3>Nos partenaires</h3>
-    <p>Profitez d'avantages sur présentation de votre carte d'adhérent Tribu-Terre !</p>
-    <br>
-    <div class="row">
-        <div class="col-sm-4">
-            <a href="https://www.facebook.com/LAtelier-203-255887947799605/">
-                <div class="well">
-                    <h4>Bar L'Atelier</h4>
-                    <p>203 rue de Bourgogne, Orléans</p>
-                    <p>
-                        <img src="<?php echo $prefixe . 'global/images/imgFacebook.svg' ?>" height="64" width="64" alt="Facebook">
-                    </p>
+    <div class="col-sm-4">
+        <h3>Nos partenaires !</h3>
+        <p>Profitez d'avantages sur présentation de votre carte d'adhérent Tribu-Terre !</p>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
 
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <!-- Ici on liste les partenaires -->
+                <div class="item active">
+                    <a href="https://www.facebook.com/LAtelier-203-255887947799605/">
+                        <img src="https://placehold.it/800x400?text=IMAGE" alt="Image">
+                    </a>
+                    <div class="carousel-caption">
+                        <a href="https://www.facebook.com/LAtelier-203-255887947799605/">
+                            <h3>Bar L'Atelier</h3>
+                        </a>
+                        <p>203 rue de Bourgogne, Orléans</p>
+                    </div>
                 </div>
+
+                <div class="item">
+                    <a href="https://www.facebook.com/Key-West-Rhumerie-318605488186473/">
+                        <img src="https://placehold.it/800x400?text=IMAGE" alt="Image">
+                    </a>
+                    <div class="carousel-caption">
+                        <a href="https://www.facebook.com/Key-West-Rhumerie-318605488186473/">
+                            <h3>Bar L'Atelier</h3>
+                        </a>
+                        <p>203 rue de Bourgogne, Orléans</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
             </a>
-        </div>
-        <div class="col-sm-4">
-            <a href="https://www.facebook.com/Key-West-Rhumerie-318605488186473/">
-                <div class="well">
-                    <h4>Bar Le Key West</h4>
-                    <p>208 rue de Bourgogne, Orléans</p>
-                    <p>
-                        <img src="<?php echo $prefixe . 'global/images/imgFacebook.svg' ?>" height="64" width="64" alt="Facebook">
-                    </p>
-                </div>
-            </a>
-        </div>
-        <div class="col-sm-4">
-            <a href="https://www.facebook.com/leptitbarc/">
-                <div class="well">
-                    <h4>Bar Le Petit Barcelone</h4>
-                    <p>218 rue de Bourgogne, Orléans</p>
-                    <p>
-                        <img src="<?php echo $prefixe . 'global/images/imgFacebook.svg' ?>" height="64" width="64" alt="Facebook">
-                    </p>
-                </div>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
             </a>
         </div>
     </div>
