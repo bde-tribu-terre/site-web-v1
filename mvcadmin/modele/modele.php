f11c50ae789dcebfd8a330b3988334056a1e0f40
Anaël iMac <anael@barodine.fr>
2020-03-30 18:53:41 +0200

    Déplacement de la fonction getConnect dans global/connect.php

--- mvcadmin/modele/modele.php
+++ mvcadmin/modele/modele.php
@@ -1,11 +1,4 @@
 <?php
-function getConnect() {
-    $connexion = new PDO('mysql:host='.SERVEUR.';dbname='.BDD,USER,PASSWORD);
-    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $connexion->query('SET NAMES UTF8');
-    return $connexion;
-}
-
 function verifConnexion($login, $mdp) {
     $connexion = getConnect();
     $requete = "SELECT idMembre, loginMembre, mdpMembre FROM Membre WHERE loginMembre=:login";
