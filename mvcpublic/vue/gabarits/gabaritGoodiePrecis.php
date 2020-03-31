57436da4e57a71805cb1eed3ab220f5436c1a758
Anaël Laptop <anael@barodine.fr>
2020-03-31 15:46:09 +0200

    Bulles d'info sur les goodies.

--- mvcpublic/vue/gabarits/gabaritGoodiePrecis.php
+++ mvcpublic/vue/gabarits/gabaritGoodiePrecis.php
@@ -13,14 +13,45 @@
                 <h3>Prix pour les non-adhérents</h3>
                 <h1><?php echo $prixNonAdherent ?>€</h1>
                 <hr>
-                <a href="#">Mais c'est quoi un adhérent ?</a>
-            </div>
-            <div class="well">
-                <h3>Quelques mots sur cette merveille...</h3>
-                <p>
-                    <?php echo $descStr ?>
-                </p>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#adherent" aria-expanded="false">
+                    Qu'est-ce qu'un adhérent ?
+                </button>
+                <div class="collapse" id="adherent">
+                    <div class="card card-body">
+                        <br>
+                        <p>Un adhérent est une personne ayant payé le prix d'adhésion auprès du bureau de Tribu-Terre.</p>
+                        <p>Le prix de l'adhésion est fixé à 5€. L'adhésion est valable pour toute la durée de l'année scolaire</p>
+                        <p>Devenir adhérent à Tribu-Terre, c'est non seulement pouvoir acheter les goodies à prix réduit, mais c'est aussi bénéficier de tarifs préférentiels chez nos partenaies !</p>
+                        <a href="#"><p>En savoir plus sur les avantages de l'adhésion !</p></a>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#recherche" aria-expanded="false">
+                    Comment acheter ?
+                </button>
+                <div class="collapse" id="recherche"">
+                    <div class="card card-body">
+                        <br>
+                        <p>Pour acheter ce goodie, rendez-vous au local Tribu-Terre !</p>
+                        <a href="#"><p>Où est notre local ?</p></a>
+                        <br>
+                        <p>Nous organisons également régulièrement des permanences dans le hall du bâtiment S !</p>
+                        <a href="#"><p>Tenez-vous informé sur nos réseaux !</p></a>
+                        <br>
+                        <p>Certains de nos goodies peuvent être achetés pendant nos évents !</p>
+                        <a href="<?php echo $prefixe . 'events/' ?>"><p>Quels sont les prochains events ?</p></a>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div><br>
+    </div><br>
+    <div class="row">
+        <div class="well">
+            <h3>Quelques mots sur cette merveille...</h3>
+            <p>
+                <?php echo $descStr ?>
+            </p>
+        </div>
+    </div><br>
+</div>
