6973c4038d0248f6d326e1f997d144452ce7e5da
Anaël iMac <anael@barodine.fr>
2020-04-06 20:22:00 +0200

    Test Ajout AG.

--- -mvc/vue/gabaritsPublic/gabaritHistorique.php
+++ -mvc/vue/gabaritsPublic/gabaritHistorique.php
@@ -1,38 +1,63 @@
 <div class="container text-center">
     <h3>Historique de l'association</h3>
     <hr>
-    <div class="row">
+    <div class="row text-justify retrait">
         <div class="col-sm-1"></div>
         <div class="col-sm-10">
-            <div class="text-justify retrait">
-                <p>
-                    Comme toute association étudiante, le bureau de Tribu-Terre évolue régulièrement grâce à
-                    l'implication de nouveaux membres ! Tous les ans, une Assemblée Générale est organisée dans laquelle
-                    les personnes composant le bureau de l'année passée démissionnent. Un vote est alors organisé et les
-                    adhérents de l'association élisent leur nouveau bureau parmi les étudiants qui souhaitent se
-                    présenter (ou se représenter) comme membre du prochain bureau. L'ensemble des personnes ainsi élues
-                    composeront le bureau de Tribu-Terre pendant un an, jusqu'à la date de la prochaine Assemblée
-                    Générale.
-                </p>
-                <p>
-                    <a href="<?php echo $prefixe . 'association/fonctionnement/' ?>">
-                        En savoir plus sur le fonctionnement de l'association
-                    </a>
-                </p>
-                <p>
-                    Ci-dessous la liste des bureaux votés à chacune des Assemblées Générales, qui tous ont su faire
-                    évoluer Tribu-Terre pour la transformer en ce qu'elle est aujourd'hui.
-                </p>
-            </div>
+            <p>
+                Comme toute association étudiante, le bureau de Tribu-Terre évolue régulièrement grâce à
+                l'implication de nouveaux membres ! Tous les ans, une Assemblée Générale est organisée dans laquelle
+                les personnes composant le bureau de l'année passée démissionnent. Un vote est alors organisé et les
+                adhérents de l'association élisent leur nouveau bureau parmi les étudiants qui souhaitent se
+                présenter (ou se représenter) comme membre du prochain bureau. L'ensemble des personnes ainsi élues
+                composeront le bureau de Tribu-Terre pendant un an, jusqu'à la date de la prochaine Assemblée
+                Générale.
+            </p>
+            <p>
+                <a href="<?php echo $prefixe . 'association/fonctionnement/' ?>">
+                    En savoir plus sur le fonctionnement de l'association
+                </a>
+            </p>
+            <p>
+                Ci-dessous la liste des bureaux votés à chacune des Assemblées Générales, qui tous ont su faire
+                évoluer Tribu-Terre pour la transformer en ce qu'elle est aujourd'hui.
+            </p>
         </div>
         <div class="col-sm-1"></div>
     </div>
     <hr>
-    <div class="row">
+    <div class="row text-left">
         <div class="col-sm-3">
+            <img src="<?php echo $prefixe . '-images/imgAG2019' ?>">
         </div>
         <div class="col-sm-9">
             <div class="well">
+                <h3>Assemblée Générale du 12 mars 2019</h3>
+                <h4>Tribu-Terre, Association des étudiants scientifiques de l'Université d'Orléans</h4>
+                <ul>
+                    <li>Riad BENCHEKRA (Président)</li>
+                    <li>Arthur CHAMPENOIS (Vice Président Général)</li>
+                    <li>Jade BEAUMONT (Secrétaire)</li>
+                    <li>Léa LUCIDOR (Vice Secrétaire)</li>
+                    <li>Jimi Marie-Sainte : Trésorier
+                        - Alan Le Quellenec : Vice trésorier
+                        - Lina Berrada : VP communication
+                        - Emma Caillet : VP Photo-communication
+                        - Chloë Rolland : VP événementiel
+                        - William Abessolo : VP Partenariats
+                        - Simon Medelli : VP Culture
+                        - Kevin Tillay : VP Jardin
+                        - Laure Gilet : CM Conférences</li>
+                </ul>
+                <hr>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#2019">
+                    Voir le post Facebook
+                </button>
+                <div class="collapse" id="2019">
+                    <div class="card card-body">
+                        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmedia%2Fset%2F%3Fset%3Da.524805124713427%26type%3D3&width=500" width="500" height="814" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
