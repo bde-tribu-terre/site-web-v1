7ad55f5f630aac704b406a386055b903acb892c3
Anaël iMac <anael@barodine.fr>
2020-04-06 16:45:11 +0200

    Carte mise à jour.

--- -mvc/vue/gabaritsPublic/gabaritOuNousTrouver.php
+++ -mvc/vue/gabaritsPublic/gabaritOuNousTrouver.php
@@ -7,7 +7,7 @@
             <div class="container text-left">
                 <p>Où nous trouver ?</p>
                 <p>Géographiquement</p>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d844.8128790394246!2d1.9420368292630361!3d47.83393189895918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDfCsDUwJzAyLjIiTiAxwrA1NiczMy4zIkU!5e1!3m2!1sfr!2sfr!4v1586183863612!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8924.851732286033!2d1.935002002916136!3d47.83939441504462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDfCsDUwJzAyLjIiTiAxwrA1NiczMy4zIkU!5e0!3m2!1sfr!2sfr!4v1586184279306!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 <p>Sur les réseaux</p>
                 <p>Liens réseaux</p>
             </div>
