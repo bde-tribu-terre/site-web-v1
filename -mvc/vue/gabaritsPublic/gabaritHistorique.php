1c50dccee21e469cd0ff6db42202db69546fcb30
Anaël iMac <anael@barodine.fr>
2020-04-06 19:51:42 +0200

    Ajout description historique.

--- -mvc/vue/gabaritsPublic/gabaritHistorique.php
+++ -mvc/vue/gabaritsPublic/gabaritHistorique.php
@@ -2,6 +2,33 @@
     <h3>Historique de l'association</h3>
     <hr>
     <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <div class="text-justify">
+                <p>
+                    Comme toute association étudiante, le bureau de Tribu-Terre évolue régulièrement grâce à
+                    l'implication de nouveaux membres ! Tous les ans, une Assemblée Générale est organisée dans laquelle
+                    les personnes composant le bureau de l'année passée démissionnent. Un vote est alors organisé et les
+                    adhérents de l'association élisent leur nouveau bureau parmi les étudiants qui souhaitent se
+                    présenter (ou se représenter) comme membre du prochain bureau. L'ensemble des personnes ainsi élues
+                    composeront le bureau de Tribu-Terre pendant un an, jusqu'à la date de la prochaine Assemblée
+                    Générale.
+                </p>
+                <p>
+                    <a href="<?php echo $prefixe . 'association/fonctionnement/' ?>">
+                        En savoir plus sur le fonctionnement de l'association
+                    </a>
+                </p>
+                <p>
+                    Ci-dessous la liste des bureaux votés à chacune des Assemblées Générales, qui tous ont su faire
+                    évoluer Tribu-Terre pour la transformer en ce qu'elle est aujourd'hui.
+                </p>
+            </div>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <hr>
+    <div class="row">
         <div class="col-sm-3">
         </div>
         <div class="col-sm-9">
