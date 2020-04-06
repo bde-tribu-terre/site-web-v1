73226a08a69ed212b1615f5180455c60f70a4efd
Anaël iMac <anael@barodine.fr>
2020-04-06 19:24:46 +0200

    Supr commentaire.

--- -mvc/vue/gabaritsPublic/gabaritContact.php
+++ -mvc/vue/gabaritsPublic/gabaritContact.php
@@ -4,34 +4,6 @@
     <div class="row">
         <div class="col-sm-3"></div>
         <div class="col-sm-6">
-            <!-- Ce n'est pas vraiment le but de cette page finalement.
-            <h3>Réseaux sociaux</h3>
-            <div class="well">
-                <h3>Facebook</h3>
-                <hr>
-                <a href="https://www.facebook.com/bdeTribuTerre/">
-                    <img src="<?php echo $prefixe . '-images/imgFacebook.svg' ?>" height="100" alt="Facebook">
-                    <h4>https://www.facebook.com/bdeTribuTerre/</h4>
-                </a>
-            </div>
-            <div class="well">
-                <h3>Instagram</h3>
-                <hr>
-                <a href="https://www.instagram.com/tribu.terre/">
-                    <img src="<?php echo $prefixe . '-images/imgInstagram.svg' ?>" height="100" alt="Instagram">
-                    <h4>https://www.instagram.com/tribu.terre/</h4>
-                </a>
-            </div>
-            <div class="well">
-                <h3>Twitter</h3>
-                <hr>
-                <a href="https://twitter.com/Tributerre45/">
-                    <img src="<?php echo $prefixe . '-images/imgTwitter.svg' ?>" height="100" alt="Twitter">
-                    <h4>https://twitter.com/Tributerre45/</h4>
-                </a>
-            </div>
-            <hr>
-            -->
             <p>Je sais trop pas comment présenter le look de cette page...</p>
             <h3>Courriel</h3>
             <div class="well">
