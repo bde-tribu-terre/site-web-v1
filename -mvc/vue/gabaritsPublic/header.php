3a2ed346cc08552787fa439b1bc3860d6923ed7c
Anaël iMac <anael@barodine.fr>
2020-04-13 02:43:17 +0200

    MAJ Header.

--- -mvc/vue/gabaritsPublic/header.php
+++ -mvc/vue/gabaritsPublic/header.php
@@ -7,29 +7,46 @@
     </div>
 </div>
 
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigationHeader"> <!-- C'est le petit bouton menu quand l'écran est trop petit -->
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-        <div class="collapse navbar-collapse" id="navigationHeader"> <!-- Et ça c'est quand l'écran est assez grand -->
-            <ul class="nav navbar-nav">
-                <li<?php if ($gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritAccueil.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe ?>">Accueil</a></li>
-                <li<?php if ($gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritPresentation.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe . 'association/' ?>">Qui sommes-nous ?</a></li>
-                <li<?php if ($gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritEvents.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe . 'events/' ?>">Évents</a></li>
-                <li<?php if ($gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritGoodies.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe . 'goodies/' ?>">Goodies</a></li>
-                <li<?php if ($gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritJournaux.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe . 'journaux/' ?>">Journaux</a></li>
-                <li<?php if ($gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritArticles.php') { echo ' class="active"'; } ?>><a href="<?php echo $prefixe . 'articles/' ?>">Articles</a></li>
-            </ul>
-            <!-- Bouton se connecter
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-            </ul>
-            -->
-        </div>
+<nav class="navbar navbar-expand-sm navbar-tribu-terre py-0">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigationHeader"> <!-- C'est le petit bouton menu quand l'écran est trop petit -->
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <div class="navbar-collapse collapse" id="navigationHeader"> <!-- Et ça c'est quand l'écran est assez grand -->
+        <ul class="nav navbar-nav">
+            <li class="nav-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritAccueil.php' ? ' active' : ''; ?>">
+                <a class="nav-link" href="<?php echo $prefixe ?>">Accueil</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Présentation <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritPresentation.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/' ?>">Qui sommes-nous ?</a></li>
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritPourquoiAdherer.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/pourquoi-adherer/' ?>">Pourquoi adhérer ?</a></li>
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritReseauxSociaux.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/reseaux-sociaux/' ?>">Réseaux sociaux</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritPartenaires.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/partenaires/' ?>">Partenaires</a></li>
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritReseauAssociatif.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/reseau-associatif/' ?>">Réseau associatif</a></li>
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritUniversite.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/universite/' ?>">Université d'Orléans</a></li>
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritPoles.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/poles/' ?>">Pôles</a></li>
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritFonctionnement.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/fonctionnement/' ?>">Fonctionnement de l'association</a></li>
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritHistorique.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/historique/' ?>">Historique de l'association</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a class="dropdown-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritContact.php' ? ' active' : ''; ?>" href="<?php echo $prefixe . 'association/contact/' ?>">Contact</a></li>
+                </ul>
+            </li>
+            <li class="nav-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritEvents.php' ? ' active' : ''; ?>">
+                <a class="nav-link" href="<?php echo $prefixe . 'events/' ?>">Évents</a>
+            </li>
+            <li class="nav-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritGoodies.php' ? ' active' : ''; ?>">
+                <a class="nav-link" href="<?php echo $prefixe . 'goodies/' ?>">Goodies</a>
+            </li>
+            <li class="nav-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritJournaux.php' ? ' active' : ''; ?>">
+                <a class="nav-link" href="<?php echo $prefixe . 'journaux/' ?>">Journaux</a>
+            </li>
+            <li class="nav-item<?php echo $gabarit == $prefixe . '-mvc/vue/gabaritsPublic/gabaritArticles.php' ? ' active' : ''; ?>">
+                <a class="nav-link" href="<?php echo $prefixe . 'articles/' ?>">Articles</a>
+            </li>
+        </ul>
     </div>
 </nav>
