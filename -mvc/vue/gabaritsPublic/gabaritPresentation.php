e07aa609e9fc6053d861bc1a05b2c63ca350c2e9
Anaël iMac <anael@barodine.fr>
2020-04-22 01:33:15 +0200

    Correction texte qui sommes-nous ?

--- -mvc/vue/gabaritsPublic/gabaritPresentation.php
+++ -mvc/vue/gabaritsPublic/gabaritPresentation.php
@@ -7,33 +7,36 @@
         <div class="col-sm-10">
             <h4 class="text-center">Tribu-Terre, une association pour les étudiants par des étudiants !</h4>
             <p>
-                Tribu-terre est une association d’étudiants ayant pour but de représenter les étudiants du COST et de
-                l'OSUC, de dynamiser la vie étudiante et de d'offfrir une dimension  culturelle, évènementielle, ou
+                Tribu-terre est une association d’étudiants ayant pour but de représenter les étudiants du CoST et de
+                l'OSUC, de dynamiser la vie étudiante et de d'offfrir une dimension culturelle, évènementielle, ou
                 encore écologique aux étudiants d’Orléans autour des sciences.
             </p>
             <p>
                 <a href="<?php echo $prefixe . 'association/universite/' ?>">
-                    Université d'Orléans
+                    En savoir plus sur l'Université d'Orléans.
                 </a>
             </p>
             <p>
-                Votre association vous représente auprès de l'administration universitaire du Cost et de l’OSUC depuis quelques années et veille chaque jour à faire évoluer ses projets pour l'ensemble des étudiants.
+                Votre association vous représente auprès de l'administration universitaire du CoST et de l’OSUC depuis
+                quelques années et veille chaque jour à faire évoluer ses projets pour l'ensemble des étudiants.
+            </p>
+            <br>
+            <p>
+                Toujours dans l’optique d’apporter du confort, du plaisir et de la science à nos étudiants, divers pôles
+                ont été créés et œuvrent actuellement.
             </p>
             <p>
-                Toujours dans l’optique d’apporter du confort, du plaisir et de la science à nos étudiants, divers pôles ont été créés et œuvrent actuellement:
+                <a href="<?php echo $prefixe . 'association/poles/' ?>">
+                    Plus d'information sur les pôles actuels de Tribu-Terre
+                </a>
             </p>
-            <ul>
-                <li>Culture</li>
-                <li>Evènementiel</li>
-                <li>Cuisine</li>
-                <li>Communication</li>
-                <li>Partenariat</li>
-                <li>Journal</li>
-                <li>Goodies</li>
-                <li>Informatique</li>
-            </ul>
+            <br>
             <p>
-                Mais il ne s’agit pas uniquement de se refermer sur nous-même et c'est dans ce but que Tribu-Terre vous représentent aussi au sein de fédérations afin d'échanger avec d'autres associations, de débattre de divers sujets et de toujours vous apporter le meilleur. Pour cela, nous adhérons à deux fédérations : d'une part Ôcampus, fédération regroupant les associations orléanaises et d'autre part la FNEB qui représente des associations scientifiques de France.
+                Mais il ne s’agit pas uniquement de se refermer sur nous-même et c'est dans ce but que Tribu-Terre vous
+                représente aussi au sein de fédérations afin d'échanger avec d'autres associations, de débattre de
+                divers sujets et de toujours vous apporter le meilleur. Pour cela, nous adhérons à deux fédérations :
+                d'une part Ôcampus, fédération regroupant les associations orléanaises, et d'autre part la FNEB, qui
+                représente des associations scientifiques de France.
             </p>
             <p>
                 <a href="<?php echo $prefixe . 'association/reseau-associatif/ocampus/' ?>">
@@ -45,8 +48,12 @@
                     Qu'est-ce que la FNEB ?
                 </a>
             </p>
+            <br>
             <p>
-                Nous disposons aussi d’un local à l’OSUC dans lequel vous êtes les bienvenus et où vous pouvez d’ailleurs retrouver les membres de notre  bureau grâce à nos évènements et permanences. Et si vous ne pouvez pas venir au local, alors vous pouvez nous suivre sur nos réseaux où vous retrouverez toute l’actualité de votre association préférée !
+                Nous disposons aussi d’un local à l’OSUC dans lequel vous êtes les bienvenus et où vous pouvez
+                d’ailleurs retrouver les membres de notre  bureau grâce à nos évènements et permanences. Et si vous ne
+                pouvez pas venir au local, alors vous pouvez nous suivre sur nos réseaux où vous retrouverez toute
+                l’actualité de votre association préférée !
             </p>
             <p>
                 <a href="<?php echo $prefixe . 'association/ou-nous-trouver/' ?>">
@@ -55,7 +62,7 @@
             </p>
             <p>
                 <a href="<?php echo $prefixe . 'association/reseaux-sociaux/' ?>">
-                    Réseaux sociaux
+                    Quels sont nos réseaux sociaux ?
                 </a>
             </p>
         </div>
