82ccbbb2b8c55a5962885ce7b413bd82716d68de
Anaël iMac <anael@barodine.fr>
2020-04-22 03:14:55 +0200

    Pôles MEP 2.

--- -mvc/vue/gabaritsPublic/gabaritPoles.php
+++ -mvc/vue/gabaritsPublic/gabaritPoles.php
@@ -11,26 +11,31 @@
         </div>
         <div class="col-sm-1"></div>
     </div>
-    <div class="row text-center">
-        <div class="col-sm-1"></div>
-        <div class="col-sm-10">
-            <!-- \/ AG 2019 \/ -->
-            <div>
-                <button class="btn btn-danger btn-block" type="button" data-toggle="collapse" data-target="#culture">
-                    Pôle Culture
-                </button>
-                <div class="collapse" id="culture">
-                    <div style="margin: 15px" class="card card-body">
-                        <div class="well text-justify retrait">
-                            <p>
-                                Texte pôle culture.
-                            </p>
+    <hr>
+    <!-- \/ Pôle Culture \/ -->
+    <div>
+        <button class="btn btn-danger btn-block" type="button" data-toggle="collapse" data-target="#culture">
+            Pôle Culture
+        </button>
+        <div class="collapse" id="culture">
+            <div style="margin: 15px" class="card card-body">
+                <div class="well">
+                    <div class="row">
+                        <h3>Pôle Culture</h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3"></div>
+                            <div class="col-sm-6 text-left retrait">
+                                <p>
+                                    Texte pôle culture.
+                                </p>
+                            </div>
+                            <div class="col-sm-3"></div>
                         </div>
                     </div>
                 </div>
-                <hr>
             </div>
         </div>
-        <div class="col-sm-1"></div>
+        <hr>
     </div>
 </div><br>
