0f570098667aa119fd5ea5abec638d037f31433f
Anaël iMac <anael@barodine.fr>
2020-04-22 16:31:12 +0200

    Boutons des pôles.

--- -mvc/vue/gabaritsPublic/gabaritPoles.php
+++ -mvc/vue/gabaritsPublic/gabaritPoles.php
@@ -24,13 +24,195 @@
                         <h3>Pôle Culture</h3>
                         <hr>
                         <div class="row">
-                            <div class="col-sm-3"></div>
-                            <div class="col-sm-6 text-left retrait">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-8 text-left retrait">
                                 <p>
                                     Texte pôle culture.
                                 </p>
                             </div>
-                            <div class="col-sm-3"></div>
+                            <div class="col-sm-2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <!-- \/ Pôle Événementiel \/ -->
+    <div>
+        <button class="btn btn-danger btn-block" type="button" data-toggle="collapse" data-target="#evenementiel">
+            Pôle Événementiel
+        </button>
+        <div class="collapse" id="evenementiel">
+            <div style="margin: 15px" class="card card-body">
+                <div class="well">
+                    <div class="row">
+                        <h3>Pôle Événementiel</h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-8 text-left retrait">
+                                <p>
+                                    Texte pôle événementiel.
+                                </p>
+                            </div>
+                            <div class="col-sm-2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <!-- \/ Pôle Cuisine \/ -->
+    <div>
+        <button class="btn btn-danger btn-block" type="button" data-toggle="collapse" data-target="#cuisine">
+            Pôle Cuisine
+        </button>
+        <div class="collapse" id="cuisine">
+            <div style="margin: 15px" class="card card-body">
+                <div class="well">
+                    <div class="row">
+                        <h3>Pôle Cuisine</h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-8 text-left retrait">
+                                <p>
+                                    Texte pôle cuisine.
+                                </p>
+                            </div>
+                            <div class="col-sm-2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <!-- \/ Pôle Communication \/ -->
+    <div>
+        <button class="btn btn-danger btn-block" type="button" data-toggle="collapse" data-target="#communication">
+            Pôle Communication
+        </button>
+        <div class="collapse" id="communication">
+            <div style="margin: 15px" class="card card-body">
+                <div class="well">
+                    <div class="row">
+                        <h3>Pôle Communication</h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-8 text-left retrait">
+                                <p>
+                                    Texte pôle communication.
+                                </p>
+                            </div>
+                            <div class="col-sm-2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <!-- \/ Pôle Partenariat \/ -->
+    <div>
+        <button class="btn btn-danger btn-block" type="button" data-toggle="collapse" data-target="#partenariats">
+            Pôle Partenariats
+        </button>
+        <div class="collapse" id="partenariats">
+            <div style="margin: 15px" class="card card-body">
+                <div class="well">
+                    <div class="row">
+                        <h3>Pôle Partenariats</h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-8 text-left retrait">
+                                <p>
+                                    Texte pôle partenariats.
+                                </p>
+                            </div>
+                            <div class="col-sm-2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <!-- \/ Pôle Journal \/ -->
+    <div>
+        <button class="btn btn-danger btn-block" type="button" data-toggle="collapse" data-target="#journal">
+            Pôle Journal
+        </button>
+        <div class="collapse" id="journal">
+            <div style="margin: 15px" class="card card-body">
+                <div class="well">
+                    <div class="row">
+                        <h3>Pôle Journal</h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-8 text-left retrait">
+                                <p>
+                                    Texte pôle journal.
+                                </p>
+                            </div>
+                            <div class="col-sm-2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <!-- \/ Pôle Goodies \/ -->
+    <div>
+        <button class="btn btn-danger btn-block" type="button" data-toggle="collapse" data-target="#goodies">
+            Pôle Goodies
+        </button>
+        <div class="collapse" id="goodies">
+            <div style="margin: 15px" class="card card-body">
+                <div class="well">
+                    <div class="row">
+                        <h3>Pôle Goodies</h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-8 text-left retrait">
+                                <p>
+                                    Texte pôle goodies.
+                                </p>
+                            </div>
+                            <div class="col-sm-2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <!-- \/ Pôle Informatique \/ -->
+    <div>
+        <button class="btn btn-danger btn-block" type="button" data-toggle="collapse" data-target="#informatique">
+            Pôle Informatique
+        </button>
+        <div class="collapse" id="informatique">
+            <div style="margin: 15px" class="card card-body">
+                <div class="well">
+                    <div class="row">
+                        <h3>Pôle Informatique</h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-8 text-left retrait">
+                                <p>
+                                    Texte pôle informatique.
+                                </p>
+                            </div>
+                            <div class="col-sm-2"></div>
                         </div>
                     </div>
                 </div>
