b820f3c2ecaa936ca0d7fdbbf6616e9d7fc12a16
Anaël iMac <anael@barodine.fr>
2020-04-22 02:08:32 +0200

    Texte fonctionnement.

--- -mvc/vue/gabaritsPublic/gabaritFonctionnement.php
+++ -mvc/vue/gabaritsPublic/gabaritFonctionnement.php
@@ -1,36 +1,79 @@
 <div class="container text-center">
     <h3>Fonctionnement de l'association</h3>
     <hr>
-    <div class="row">
+    <div class="row text-justify retrait">
         <div class="col-sm-1"></div>
         <div class="col-sm-10">
-            <div class="text-justify">
-                <p>
-                    Comment fonctionne l'association ?
-                    Comment est-elle gérée ?
-                    Quels sont les différents acteurs ? (adhérents/membres/bureau/...)
-                    Qu'est-ce que l'AG ?
-                    Il peut y avoir des liens, des images, des trucs dans le texte etc...
-                </p>
-                <p>
-                    (Texte temporaire que j'avais fait)
-                </p>
-                <p>
-                    Les statuts de l'association sont, en quelque sorte, le règlement de conduite que doit suivre l'association et ses
-                    membres.<br>
-                    Il s'agit d'un ensemble d'articles à valeur légale qui détaillent comment doit s'organiser la direction de
-                    Tribu-Terre par ses membres actifs.<br>
-                    Ils sont consultables à tout moment par n'importe qui. Ils ne peuvent cependant être modifiés que lors d'une
-                    Assemblée Générale Extraordinaire<br>
-                    (ce qui est détaillé dans les statuts eux-mêmes).
-                </p>
-                <h3>
-                    Les statuts actuels de l'association, modifiés pour la dernière fois le 12 Mars 2019, sont disponibles ici :
-                </h3>
-                <h3>
-                    <a href="<?php echo $prefixe . 'association/fonctionnement/2019-03-12-Statuts.pdf' ?>">Consulter les statuts.</a>
-                </h3>
-            </div>
+            <p>
+                Tribu-Terre fonctionne de la même manière que la pluparts des associations étudiantes (à ne pas
+                confondre avec les corporations ou les syndicats par exemple), c'est-à-dire qu'elle se compose d'un
+                bureau restreint , de membres actifs et de membres adhérents.
+            </p>
+            <p>
+                Il existe plusieurs types d'acteurs au sein de l'association,  décrits dans nos (statuts). Mais voici
+                une petite explication plus « pratique » :
+            </p>
+            <ul style="text-indent: 0">
+                <li>
+                    Vous pouvez intégrer l’association en adhérant au cours de l'année universitaire, ce qui permet de
+                    profiter de nombreux avantages. Actuellement, le montant de la côtisation des adhérents est de 5€.
+                    <a href="<?php echo $prefixe . 'association/pourquoi-adherer/' ?>">Pourquoi adhérer ?</a>
+                </li>
+                <li>
+                    Vous pouvez aussi faire un quelconque don financier ou matériel à l’association et devenir membre
+                    bienfaiteur.
+                </li>
+            </ul>
+            <p>
+                Vous souhaitez vous investir plus concrètement dans l'association et devenir membre du bureau ?
+            </p>
+            <ul style="text-indent: 0">
+                <li>
+                    Vous pouvez participer à l’activité de différents pôles en devenant membre actif.
+                </li>
+                <li>
+                    Certains membres actifs peuvent aussi être nommés « responsable d'activité » par le  Conseil
+                    d'Administration et devenir des Chargés de mission ou des Vice-présidents affiliés à des pôles en
+                    particulier.
+                </li>
+            </ul>
+            <p>
+                Pour cela, il suffit d’être adhérent, de
+                <a href="<?php echo $prefixe . 'association/contact/' ?>">nous contacter</a> et de venir à un conseil
+                d’administration pour se présenter et annoncer sa candidature. Lors de la prochaine réunion, un vote
+                aura lieu pour vous intégrer officiellement à votre nouveau poste.
+            </p>
+            <ul style="text-indent: 0">
+                <li>
+                    Enfin, chaque association est composé d'un bureau restreint. Il s'agit du Président, du
+                    Vice-Président Général, du Trésorier, du Vice-Trésorier, du Secrétaire et du Vice-Secrétaire. Ils
+                    sont les responsables législatifs et financiers de l'association.
+                </li>
+            </ul>
+            <p>
+                Régulièrement, tous ces membres se réunissent pour faire un bilan des projets passés et futurs des
+                <a href="<?php echo $prefixe . 'association/poles/' ?>">différents pôles</a>, prendre des décisions
+                importantes, voter de nouveaux membres mais aussi faire un bilan trésorerie où les trésorier et
+                vice-trésorier réccapitulent les dépenses effectuées et à venir. C'est ce que l'on appelle un Conseil
+                d'Administration.
+            </p>
+            <p>
+                Une fois par an, nous organison aussi une Assemblée Générale Ordinaire. Au cours de celle-ci, les
+                membres du Conseil d'Administration (adhérents, membres bienfaiteurs, membres actifs et bureau
+                restreint) se réunissent pour faire le bilan des activités de l’association sur l’année, valider les
+                dépenses du mandat et en général, procéder au renouvellement  complet des membres du Conseil
+                d'Administration.
+            </p>
+            <p>
+                Cependant, il est aussi possible de faire une Assemblée Générale Extraordinaire, lorsqu'il faut modifier
+                les statuts, ou modifier le Conseil d'Administration (par exemple lors de la dissolution de
+                l'association).
+            </p>
+            <p>
+                Tribu-Terre est une association à but non lucratif. Cela signifie que tout l'argent récupéré par
+                l'association par l'intermédiaire des adhésions, des divers ventes ou encore de subventions est
+                obligatoirement réutilisé pour les étudiants.
+            </p>
         </div>
         <div class="col-sm-1"></div>
     </div>
