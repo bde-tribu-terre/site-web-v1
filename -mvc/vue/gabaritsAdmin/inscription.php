5ab92dce6b953847433d54a7d8844801d4533e90
Anaël iMac <anael@barodine.fr>
2020-04-28 17:45:46 +0200

    Finalisation du gabarit.

--- -mvc/vue/gabaritsAdmin/inscription.php
+++ -mvc/vue/gabaritsAdmin/inscription.php
@@ -15,24 +15,32 @@
     <div class="row">
         <div class="col-sm-12">
             <div class="well">
-                <h3>Connexion</h3>
+                <h3>Inscription</h3>
                 <hr>
                 <form id="formConnexion" method="post">
                     <p> <!-- Clé d'inscription -->
                         <label for="formInscription_cleInscription">Clé d'inscription</label>
                         <input class="form-control" id="formInscription_cleInscription" type="password" name="formInscription_cleInscription" placeholder="Saisir la clé d'inscription">
                     </p>
-                    <p> <!-- Mot de passe -->
-                        <label for="formInscription_login">Mot de passe</label>
-                        <input class="form-control" id="formInscription_login" type="password" name="formInscription_login" placeholder="Saisir le login">
+                    <p> <!-- Prénom -->
+                        <label for="formInscription_prenom">Prénom</label>
+                        <input class="form-control" id="formInscription_prenom" type="text" name="formInscription_prenom" placeholder="Saisir votre prénom">
+                    </p>
+                    <p> <!-- Nom -->
+                        <label for="formInscription_nom">Nom</label>
+                        <input class="form-control" id="formInscription_nom" type="text" name="formInscription_nom" placeholder="Saisir votre nom de famille">
+                    </p>
+                    <p> <!-- Login -->
+                        <label for="formInscription_login">Login</label>
+                        <input class="form-control" id="formInscription_login" type="text" name="formInscription_login" placeholder="Choisir le login">
                     </p>
                     <p> <!-- Mot de passe -->
                         <label for="formInscription_mdp">Mot de passe</label>
-                        <input class="form-control" id="formInscription_mdp" type="password" name="formInscription_mdp" placeholder="Saisir votre mot de passe">
+                        <input class="form-control" id="formInscription_mdp" type="password" name="formInscription_mdp" placeholder="Choisir un mot de passe">
                     </p>
                     <p> <!-- Valider le mot de passe -->
                         <label for="formInscription_valierMdp">Recopier le mot de passe</label>
-                        <input class="form-control" id="formInscription_valierMdp" type="password" name="formInscription_valierMdp" placeholder="Recopier votre mot de passe">
+                        <input class="form-control" id="formInscription_valierMdp" type="password" name="formInscription_valierMdp" placeholder="Recopier le mot de passe">
                     </p>
                     <hr>
                     <p> <!-- S'inscrire -->
