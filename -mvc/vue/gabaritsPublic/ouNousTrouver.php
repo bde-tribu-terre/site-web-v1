3f558e1bead47427c0268dfcc93799dea2dc5acd
Anaël iMac <anael@barodine.fr>
2020-05-11 19:50:37 +0200

    Page où nous trouver finalisée.

--- -mvc/vue/gabaritsPublic/ouNousTrouver.php
+++ -mvc/vue/gabaritsPublic/ouNousTrouver.php
@@ -4,32 +4,45 @@
     <div class="row">
         <!-- Auteur : Anaël BARODINE. -->
         <div class="col-sm-1"></div>
-        <div class="col-sm-10">
-            <div class="text-justify">
-                <p>
-                    En plus d'une présence en ligne sur nos
-                    <a href="<?php echo RACINE . 'association/reseaux-sociaux/' ?>">réseaux sociaux</a>, Tribu-Terre
-                    dispose aussi d'un local se situant à l'OSUC. Il se trouve dans la salle 4 après la cafétéria. Le
-                    logo de Tribu-Terre se trouve sur la porte, vous ne pouvez pas le rater !
-                </p>
-                <iframe class="arrondi ombre text-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8924.851732286033!2d1.935002002916136!3d47.83939441504462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDfCsDUwJzAyLjIiTiAxwrA1NiczMy4zIkU!5e0!3m2!1sfr!2sfr!4v1586184279306!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                <p>
-                    Il s'agit d'une salle de repos ouverte à tous dans laquelle vous pourrez rencontrer des membres du
-                    bureau de l'association qui seront en capacité de vous renseigner sur cette dernière, sur ses
-                    différentes actions ou ses différents événements. Vous pourrez ainsi demander à
-                    <a href="<?php echo RACINE . 'association/pourquoi-adherer/' ?>">devenir adhérent</a>, acheter votre
-                    prévente à un <a href="<?php echo RACINE . 'events/' ?>">événement</a>, prendre le dernier
-                    exemplaire du <a href="<?php echo RACINE . 'journaux/' ?>">journal Omni-Sciences</a> (qui est
-                    totalement gratuit), ou bien acheter un de
-                    <a href="<?php echo RACINE . 'goodies/' ?>">nos goodies</a> (en fonction des disponibilités).
-                </p>
-                <p>
-                    En outre, le local a également une fonction de salle de pause. Vous pourrez y trouver trois
-                    micro-ondes en libre utilisation, une bouilloire, du thé et du café gratuits, des tables et des
-                    canapés qui feraient oublier qu'on est à la fac pour travailler. C'est le rendez-vous méridien
-                    obligatoire de tous les étudiants à l'OSUC !
-                </p>
-            </div>
+        <div class="col-sm-10 text-justify retrait">
+            <p>
+                En plus d'une présence en ligne sur nos
+                <a href="<?php echo RACINE . 'association/reseaux-sociaux/' ?>">réseaux sociaux</a>, Tribu-Terre
+                dispose aussi d'un local se situant à l'OSUC. Il se trouve dans la salle 4 après la cafétéria. Le
+                logo de Tribu-Terre se trouve sur la porte, vous ne pouvez pas le rater !
+            </p>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <br>
+            <iframe class="img-arrondi ombre text-center" height="450" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8924.851732286033!2d1.935002002916136!3d47.83939441504462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDfCsDUwJzAyLjIiTiAxwrA1NiczMy4zIkU!5e0!3m2!1sfr!2sfr!4v1586184279306!5m2!1sfr!2sfr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <br><br>
+        </div>
+        <div class="col-sm-2"></div>
+    </div>
+    <div class="row">
+        <!-- Auteur : Anaël BARODINE. -->
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10 text-justify retrait">
+            <p>
+                Il s'agit d'une salle de repos ouverte à tous, dans laquelle vous pourrez rencontrer des membres du
+                bureau de l'association qui seront en capacité de vous renseigner sur cette dernière, ses différentes
+                actions ou ses différents événements. Vous pourrez ainsi demander à
+                <a href="<?php echo RACINE . 'association/pourquoi-adherer/' ?>">devenir adhérent</a>, acheter votre
+                prévente à un <a href="<?php echo RACINE . 'events/' ?>">événement</a>, prendre le dernier
+                exemplaire du <a href="<?php echo RACINE . 'journaux/' ?>">journal Omni-Sciences</a> (qui est
+                totalement gratuit), ou bien acheter un de
+                <a href="<?php echo RACINE . 'goodies/' ?>">nos goodies</a> (en fonction des disponibilités).
+            </p>
+            <p>
+                En outre, le local a également une fonction de salle de pause. Vous pourrez y trouver trois
+                micro-ondes en libre utilisation, une bouilloire, du thé et du café gratuits, des tables et des
+                canapés qui feraient oublier qu'on est à la fac pour travailler. C'est le rendez-vous méridien
+                obligatoire de tous les étudiants à l'OSUC !
+            </p>
         </div>
         <div class="col-sm-1"></div>
     </div>
