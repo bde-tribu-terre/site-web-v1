fee163a7b70556cb28890509c1bd4c3d36ed1e02
Anaël iMac <anael@barodine.fr>
2020-05-22 16:06:13 +0200

    Présentation plus condensée.

--- -mvc/vue/gabaritsPublic/presentation.php
+++ -mvc/vue/gabaritsPublic/presentation.php
@@ -5,7 +5,9 @@
         <!-- Auteur : Jade BEAUMONT. -->
         <div class="col-sm-1"></div>
         <div class="col-sm-10">
-            <h4 class="text-center">Tribu-Terre, une association pour les étudiants par des étudiants !</h4>
+            <h4 class="text-center" style="text-indent: 0;">
+                Tribu-Terre, une association pour les étudiants par des étudiants !
+            </h4>
             <br>
             <p>
                 Tribu-terre est une association d’étudiants ayant pour but de représenter les étudiants du CoST et de
@@ -15,56 +17,29 @@
             <p>
                 Votre association vous représente auprès de l'administration universitaire du CoST et de l’OSUC depuis
                 quelques années et veille chaque jour à faire évoluer ses projets pour l'ensemble des étudiants.
-            </p>
-            <p>
-                <a href="<?php echo RACINE . 'association/universite/' ?>">
-                    En savoir plus sur l'Université d'Orléans.
+                <a href="<?php echo RACINE . 'association/universite/' ?>">En savoir plus sur l'Université d'Orléans.
                 </a>
             </p>
-            <br>
             <p>
-                Toujours dans l’optique d’apporter du confort, du plaisir et de la science à nos étudiants, divers pôles
-                ont été créés et œuvrent actuellement.
+                Toujours dans l’optique d’apporter du confort, du plaisir et de la science à nos étudiants,
+                <a href="<?php echo RACINE . 'association/poles/' ?>">divers pôles</a> ont été créés et œuvrent
+                actuellement.
             </p>
-            <p>
-                <a href="<?php echo RACINE . 'association/poles/' ?>">
-                    Plus d'information sur les pôles actuels de Tribu-Terre
-                </a>
-            </p>
-            <br>
             <p>
                 Mais il ne s’agit pas uniquement de se refermer sur nous-même et c'est dans ce but que Tribu-Terre vous
                 représente aussi au sein de fédérations afin d'échanger avec d'autres associations, de débattre de
                 divers sujets et de toujours vous apporter le meilleur. Pour cela, nous adhérons à deux fédérations :
-                d'une part Ôcampus, fédération regroupant les associations orléanaises, et d'autre part la FNEB, qui
-                représente des associations scientifiques de France.
+                d'une part <a href="<?php echo RACINE . 'association/reseau-associatif/ocampus/' ?>">ÔCampus ?</a>,
+                fédération regroupant les associations orléanaises, et d'autre part la
+                <a href="<?php echo RACINE . 'association/reseau-associatif/fneb/' ?>">FNEB</a>, qui représente des
+                associations scientifiques de France.
             </p>
             <p>
-                <a href="<?php echo RACINE . 'association/reseau-associatif/ocampus/' ?>">
-                    Qu'est-ce qu'ÔCampus ?
-                </a>
-            </p>
-            <p>
-                <a href="<?php echo RACINE . 'association/reseau-associatif/fneb/' ?>">
-                    Qu'est-ce que la FNEB ?
-                </a>
-            </p>
-            <br>
-            <p>
-                Nous disposons aussi d’un local à l’OSUC dans lequel vous êtes les bienvenus et où vous pouvez
-                d’ailleurs retrouver les membres de notre  bureau grâce à nos évènements et permanences. Et si vous ne
-                pouvez pas venir au local, alors vous pouvez nous suivre sur nos réseaux où vous retrouverez toute
-                l’actualité de votre association préférée !
-            </p>
-            <p>
-                <a href="<?php echo RACINE . 'association/ou-nous-trouver/' ?>">
-                    Où nous trouver ?
-                </a>
-            </p>
-            <p>
-                <a href="<?php echo RACINE . 'association/reseaux-sociaux/' ?>">
-                    Quels sont nos réseaux sociaux ?
-                </a>
+                Nous disposons aussi d’<a href="<?php echo RACINE . 'association/ou-nous-trouver/' ?>">un local à l’OSUC
+                </a> dans lequel vous êtes les bienvenus et où vous pouvez d’ailleurs retrouver les membres de notre
+                bureau grâce à nos évènements et permanences. Et si vous ne pouvez pas venir au local, alors vous pouvez
+                nous suivre sur<a href="<?php echo RACINE . 'association/reseaux-sociaux/' ?>">nos réseaux</a> où vous
+                retrouverez toute l’actualité de votre association préférée !
             </p>
         </div>
         <div class="col-sm-1"></div>
