33148ce5929e48f4dadf78cd8104e74a4a51ba4c
Anaël iMac <anael@barodine.fr>
2020-05-22 18:21:50 +0200

    Rédaction page "pourquoi adhérer ?".

--- -mvc/vue/gabaritsPublic/pourquoiAdherer.php
+++ -mvc/vue/gabaritsPublic/pourquoiAdherer.php
@@ -2,18 +2,63 @@
     <h3>Pourquoi adhérer ?</h3>
     <hr>
     <div class="row">
+        <!-- Auteur : Anaël BARODINE. -->
         <div class="col-sm-1"></div>
         <div class="col-sm-10">
-            <div class="text-justify">
-                <p>
-                    Explications : ça coûte 5€, valable l'année universitaire.<br>
-                    Réductions chez nos partenaires (lien vers page partenaires qui affiche les prix proposés)<br>
-                    Réductions sur nos goodies (lien vers les goodies)<br>
-                    Réducitons sur les préventes soirées (& explication c'est quoi une prévente)<br>
-                    Soutenir l'association.<br>
-                    Une fois cela fait, image quelqu'un avec sa carte d'adhérent ?
-                </p>
-            </div>
+            <p class="text-justify retrait">
+                Adhérer à Tribu-Terre, c'est payer 5€ pour bénéficier de divers avantages pendant toute l'année
+                universitaire en cours.
+            </p>
+            <p class="text-justify retrait">
+                Une fois adhérent, il vous sera décerné une carte nominative attestant votre adhésion. C'est sur
+                présentation de cette dernières que vous pourrez profiter des avantages.
+            </p>
+            <h4 class="text-center">Chez nos partenaires</h4>
+            <p class="text-justify retrait">
+                L'association Tribu-Terre négocie chaque année des partenariats. Actuellement, trois partenaires
+                permettent aux adhérents de bénéficier de tarifs préférentiels sur leur carte.
+            </p>
+            <p class="text-justify retrait">
+                <a href="<?php echo RACINE . 'association/partenaires/' ?>">
+                    Plus de détails sur les partenaires et les tarifs.
+                </a>
+            </p>
+            <h4 class="text-center">Pour nos événements</h4>
+            <p class="text-justify retrait">
+                Les événements organisés par Tribu-Terre sont toujours ouverts à tous. Malgré tout, les adhérents
+                disposent de différents avantages dépendants de la nature de l'événement.
+            </p>
+            <p class="text-justify retrait">
+                Lors des soirées organisées par l'association, une "pré-vente" sera proposée et permettra lors de la
+                soirée de profiter de tarifs préférentiels sur les consommation (notamment au sein d'établissements qui
+                ne sont pas partenaires à l'année avec Tribu-Terre). Dans ce cas là, le prix de cette pré-vente sera
+                inférieure sur présentation de la carte d'adhérent.
+            </p>
+            <p class="text-justify retrait">
+                Chaque hiver, Tribu-Terre s'associe avec d'autres associations étudiantes du campus pour organiser un
+                week-end au ski. Ici, bien que le prix du voyage soit le même pour tous, les adhérents sont prioritaires
+                sur les non-adhérents s'il y a plus de demandes que de places.
+            </p>
+            <p class="text-justify retrait">
+                <a href="<?php echo RACINE . 'events/' ?>">
+                    Voir la liste des événements organisés prochainement.
+                </a>
+            </p>
+            <h4 class="text-center">Pour nos goodies</h4>
+            <p class="text-justify retrait">
+                Votre association Tribu-Terre vend différents goodies au cours de l'année universitaire, dans le but de
+                s'auto-financier ainsi que pour permettre une rivalité très amicale entre les différentes associations
+                étudiantes existantes, contribuant à la vie de campus.
+            </p>
+            <p class="text-justify retrait">
+                Pour chacun d'entre eux, il existe un prix pour les non-adhérents, et un prix qui est inférieur pour les
+                adhérents sur présentation de la carte d'adhérent lors de la transaction.
+            </p>
+            <p class="text-justify retrait">
+                <a href="<?php echo RACINE . 'goodies/' ?>">
+                    Voir la liste des goodies actuellement disponibles.
+                </a>
+            </p>
         </div>
         <div class="col-sm-1"></div>
     </div>
