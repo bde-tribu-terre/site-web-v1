5894c53ff41605485e0bcf676f08f96525cb8590
Anaël iMac <anael@barodine.fr>
2020-06-02 18:18:35 +0200

    Ajout contenu de la page partenariats.

--- -mvc/vue/gabaritsPublic/partenaires.php
+++ -mvc/vue/gabaritsPublic/partenaires.php
@@ -2,13 +2,45 @@
     <h3>Partenaires</h3>
     <hr>
     <div class="row">
-        <!-- Auteur : Xxxx XXXX. -->
+        <!-- Auteur : William ABESSOLO. -->
         <div class="col-sm-1"></div>
         <div class="col-sm-10">
             <p class="text-justify retrait">
-                Partenaires.
+                Actuellement, l'association Tribu-Terre a quatre partenariats :
             </p>
+            <ul class="text-justify liste-retrait">
+                <li>
+                    <a href="https://www.facebook.com/LAtelier-203-255887947799605/">L'Atelier</a> fait profiter les
+                    adhérents à l'association de tarifs préférentiels sur présentation de la carte d'adhérent
+                    Tribu-Terre sur une sélection de boissons.
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/Key-West-Rhumerie-318605488186473/">Le Key-West</a> fait profiter
+                    les adhérents à l'association de tarifs préférentiels sur présentation de la carte d'adhérent
+                    Tribu-Terre sur une sélection de boissons.
+                </li>
+                <li> <!-- Depuis 02/2019 -->
+                    <a href="https://maestro-snack-orleans.eatbu.com/?lang=fr">Le Maestro</a> fait profiter les
+                    adhérents à l'association de tarifs préférentiels sur présentation de la carte d'adhérent
+                    Tribu-Terre sur les formules proposés dans le restaurant.
+                </li>
+                <li> <!-- Depuis 01/2020 -->
+                    <a href="https://www.facebook.com/secretpremieres/">Secret Premieres</a> permet à tous les étudiants
+                    de profiter d'avant premières au cinéma ainsi que de bons plans divers.
+                </li>
+            </ul>
         </div>
         <div class="col-sm-1"></div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-sm-12">
+            <a href="<?php echo IMAGES . 'imgBrochurePartenariats.jpg' ?>">
+                <img class="img-arrondi ombre"
+                     src="<?php echo IMAGES . 'imgBrochurePartenariats.jpg' ?>"
+                     alt="imgBrochurePartenariats"
+                >
+            </a>
+        </div>
     </div>
 </div><br>
