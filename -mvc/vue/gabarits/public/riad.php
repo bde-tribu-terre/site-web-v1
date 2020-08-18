34e89057799f271344edc840379843aca45e88bd
Anaël iMac <anael@barodine.fr>
2020-08-18 22:07:41 +0200

    Supr page Riad (car je pense que c'est bon).

--- -mvc/vue/gabarits/public/riad.php
+++
@@ -1,7 +0,0 @@
-<div class="container text-center">
-    <div class="row">
-        <div class="col-sm-12">
-            <img class="img-arrondi ombre" src="<?php echo IMAGES . 'imgRebusRiad.png' ?>" alt="Rébus Riad">
-        </div>
-    </div>
-</div><br>
