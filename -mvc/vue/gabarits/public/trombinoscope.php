8fdd6c8cc885ab1987e478491d4fb064e4745acf
Anaël iMac <anael@barodine.fr>
2020-11-15 16:16:19 +0100

    Ajout trombinoscope.

---
+++ -mvc/vue/gabarits/public/trombinoscope.php
@@ -0,0 +1,406 @@
+<div class="container text-center">
+    <h3>Trombinoscope</h3>
+    <hr>
+    <!-- Bureau restreint -->
+    <div>
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <h1></h1>
+                <h3>Bureau restreint</h3>
+                <p class="text-justify retrait">
+                    Description Bureau restreint.
+                </p>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+        <button class="btn btn-var btn-block" type="button" data-toggle="collapse" data-target="#restreint">
+            Afficher les membres
+        </button>
+        <div class="collapse" id="restreint">
+            <br>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>Président</h3>
+                        <h4>Simon <span class="pc">Medelli</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgSimon.jpg" ?>"
+                                alt="imgSimon"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>Secrétaire</h3>
+                        <h4>Warren <span class="pc">Demuylder</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgPhotoPlaceholder.jpg" ?>"
+                                alt="imgWarren"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>Trésorier</h3>
+                        <h4>Maxence <span class="pc">Piat</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgMaxence.jpg" ?>"
+                                alt="imgMaxence"
+                        >
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>Vice Président</h3>
+                        <h4>Clément <span class="pc">Guillemet</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgClement.jpg" ?>"
+                                alt="imgClement"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>Vice Trésorier</h3>
+                        <h4>Anaël <span class="pc">Barodine</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgAnael.jpg" ?>"
+                                alt="imgAnael"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-2"></div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <!-- Pôle communication -->
+    <div>
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <h1>🎤</h1>
+                <h3>Pôle communication</h3>
+                <!-- Auteur : Lia-May DECOBECQ. -->
+                <p class="text-justify retrait">
+                    Notre rôle, en tant que pôle communication, est de vous transmettre nos informations pour que vous
+                    soyez toujours au courant des nouveautés de Tribu-Terre. Nous essayons au mieux de vous divertir et
+                    d’être présents pour vous afin de relayer les actualités du CoST et de nos divers évènements
+                    (soirées, conférences...), vous proposer des contenus variés et des partages qui pourraient vous
+                    intéresser, ainsi que tout ce qui est bon à savoir sur Tribu-Terre.
+                </p>
+                <p class="text-justify retrait">
+                    Nous sommes présents sur trois réseaux :
+                    <a href="https://www.facebook.com/bdeTribuTerre/">Facebook</a>,
+                    <a href="https://instagram.com/tribu.terre?igshid=1nskuzt0d9nhb">Instagram</a> et
+                    <a href="https://twitter.com/Tributerre45?s=09">Twitter</a>.
+                </p>
+                <p class="text-justify retrait">
+                    Ce pôle est essentiel à toute association, il est le lien entre vous, chers étudiants et les membres
+                    de votre association dévouée. Il vous ouvre à un monde d’activités variées : affiches, stories
+                    Instagram et Twitter et publications de tous types. De quoi prendre plaisir à faire de beaux textes
+                    et visuels et échanger un peu de nos idées, nos inspirations, nos passions avec vous.
+                </p>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+        <button class="btn btn-var btn-block" type="button" data-toggle="collapse" data-target="#communication">
+            Afficher les membres
+        </button>
+        <div class="collapse" id="communication">
+            <br>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>VP Communication</h3>
+                        <h4>Lia-May <span class="pc">Decobecq</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgLia.jpg" ?>"
+                                alt="imgLia"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>VP Communication</h3>
+                        <h4>Claire <span class="pc">Lallemand</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgClaire.jpg" ?>"
+                                alt="imgClaire"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>VP Communication</h3>
+                        <h4>Théo <span class="pc">Franclet</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgPhotoPlaceholder.jpg" ?>"
+                                alt="imgTheoF"
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <!-- Pôle journal -->
+    <div>
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <h1>🗞</h1>
+                <h3>Pôle journal</h3>
+                <!-- Auteur : Jade BEAUMONT. -->
+                <p class="text-justify retrait">
+                    L'association Tribu-Terre possède son propre journal, Omni-Sciences. Il s'agit d'un journal écrit
+                    par des étudiants et pour des étudiants. Son but est d'être une plateforme de diffusion de
+                    vulgarisation scientifique mais aussi de promotion de la vie étudiante. Le projet est né en Janvier
+                    2019 et depuis, nous sortons un numéro tous les mois.
+                </p>
+                <p class="text-justify retrait">
+                    Ces journaux sont disponibles à la fois en version papier dans divers points de distributions (BU,
+                    Bâtiment S, local de l'association, etc...) mais aussi en version numérique, publiée sur les réseaux
+                    sociaux et <a href="<?php echo RACINE . 'journaux/' ?>">dans la rubrique "Journaux" du site internet
+                    </a>.
+                </p>
+                <p class="text-justify retrait">
+                    Si vous aussi vous souhaitez mettre la main à la pâte et apporter votre contribution au journal, que
+                    ce soit pour une seule fois ou régulièrement, alors contactez notre équipe à l'adresse mail
+                    suivante :
+                </p>
+                <div id="mailJournal_div" style="word-wrap: break-word;">
+                    <p id="mailJournal_base64">
+                        dHJpYnV0ZXJyZS5qb3VybmFsQGdtYWlsLmNvbQ==
+                    </p>
+                </div>
+                <div class="text-center">
+                    <button
+                            id="mailJournal_button"
+                            class="btn btn-var text-center"
+                            onclick="decoder('mailJournal', 'p');"
+                    >
+                        Cliquer pour décrypter
+                    </button>
+                </div>
+                <p class="text-justify retrait">
+                    Vous pouvez écrire sur n'importe quel sujet qui ait un rapport avec la vulgarisation scientifique ou
+                    la vie étudiante. Et si jamais vous avez besoin d'aide pour trouver un sujet ou même écrire votre
+                    article, l'équipe d'Omni-Sciences est là pour ça !
+                </p>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+        <button class="btn btn-var btn-block" type="button" data-toggle="collapse" data-target="#journal">
+            Afficher les membres
+        </button>
+        <div class="collapse" id="journal">
+            <br>
+            <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>VP Journal</h3>
+                        <h4>Chloé <span class="pc">Guillin</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgChloe.jpg" ?>"
+                                alt="imgChloe"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-4"></div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <!-- Pôle goodies -->
+    <div>
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <h1>👕</h1>
+                <h3>Pôle Goodies</h3>
+                <!-- Auteur : Maxence PIAT. -->
+                <p class="text-justify retrait">
+                    Tribu-Terre propose plusieurs types de goodies au nom de l’association comme des pulls, des pins,
+                    des stickers et même des goodies à destination des étudiants comme des trousses à dissection ou des
+                    marteaux de géologue.
+                </p>
+                <p class="text-justify retrait">
+                    Les membres de ce pôle ont comme rôle de démarcher des entreprises, comparer les devis et choisir
+                    avec qui travailler, au meilleur prix et à la meilleure qualité. De plus ils sont en recherche
+                    constante de nouveaux goodies à proposer aux étudiants.
+                </p>
+                <p class="text-justify retrait">
+                    <a href="<?php echo RACINE . 'goodies/' ?>">La liste complète ainsi que les prix des goodies est
+                    disponible sur le site internet.</a>
+                </p>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+        <button class="btn btn-var btn-block" type="button" data-toggle="collapse" data-target="#goodies">
+            Afficher les membres
+        </button>
+        <div class="collapse" id="goodies">
+            <br>
+            <div class="row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>VP Goodies</h3>
+                        <h4>Léa <span class="pc">Fullatier</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgPhotoPlaceholder.jpg" ?>"
+                                alt="imgLea"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>VP Goodies</h3>
+                        <h4>Hugo <span class="pc">Vedie</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgHugo.jpg" ?>"
+                                alt="imgHugo"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-2"></div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <!-- Pôle Jardin -->
+    <div style="display: none;">
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <h1>🥦</h1>
+                <h3>Pôle Jardin</h3>
+                <p class="text-justify retrait">
+                    Description pôle jardin
+                </p>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+        <button class="btn btn-var btn-block" type="button" data-toggle="collapse" data-target="#jardin">
+            Afficher les membres
+        </button>
+        <div class="collapse" id="jardin">
+            <br>
+            <div class="row">
+                <div class="col-sm-12"></div>
+            </div>
+        </div>
+    </div>
+    <hr style="display: none;">
+    <!-- Pôle évents -->
+    <div>
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <h1>🎫</h1>
+                <h3>Pôle Événementiel</h3>
+                <!-- Auteur : Théo LAM. -->
+                <p class="text-justify retrait">
+                    Le pôle évent a pour rôle la création, l’organisation et la gestion des divers
+                    <a href="<?php echo RACINE . 'events/' ?>">événements</a> qui rythment l’année universitaire : le
+                    pique-nique et la soirée d’intégration marquant le début des études, des soirées à thème tout au
+                    long de l’année (Halloween, St Valentin etc…) ou encore la soirée de désintégration pour fêter la
+                    fin des examens de fin d’année.
+                </p>
+                <p class="text-justify retrait">
+                    Mais ce pôle s’occupe aussi de l’organisation d’un des projets phare de l’association et qui nous
+                    tient tout particulièrement à cœur : le Parrainage. Chaque année, les nouveaux étudiants en première
+                    année peuvent s’inscrire, par le biais d’un formulaire savamment élaboré en collaboration avec le
+                    pôle communication, pour  être parrainé par un étudiant plus avancé dans ses études, en accord avec
+                    leurs projets d’études, leurs attentes et leurs personnalités ! Nous prenons ensuite le relais pour
+                    former les paires ou les trios et organiser une soirée mémorable où les « fillots » devront
+                    retrouver leurs parrains grâce à des indices.
+                </p>
+                <p class="text-justify retrait">
+                    Mais au-delà d’une simple organisation de soirées, notre objectif est de créer et renforcer la
+                    cohésion entre tous les étudiants en sciences, pour que tous puissent garder des souvenirs
+                    inoubliables de leurs années d’étude à Orléans. Pour cela, toute activité proposée est travaillée en
+                    amont en collaboration avec d’autres pôles de l’association comme par exemple les pôles
+                    communication et goodies entre autres, pour que tout se déroule dans les meilleures conditions
+                    possibles.
+                </p>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+        <button class="btn btn-var btn-block" type="button" data-toggle="collapse" data-target="#event">
+            Afficher les membres
+        </button>
+        <div class="collapse" id="event">
+            <br>
+            <div class="row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>VP Événementiel</h3>
+                        <h4>Théo <span class="pc">Lam</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgTheoL.jpg" ?>"
+                                alt="imgTheoL"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="well">
+                        <h3>VP Événementiel & Administrateur ÔCampus</h3>
+                        <h4>Ludwig <span class="pc">Soros</span></h4>
+                        <h5>Études</h5>
+                        <hr>
+                        <img
+                                class="img-responsive arrondi"
+                                src="<?php echo IMAGES . "imgLudwig.jpg" ?>"
+                                alt="imgLudwig"
+                        >
+                    </div>
+                </div>
+                <div class="col-sm-2"></div>
+            </div>
+        </div>
+    </div>
+</div><br>
