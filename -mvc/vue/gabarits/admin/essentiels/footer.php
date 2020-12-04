038ddcf147ea09aa1f519a325b7598d67d3c724b
Anaël iMac <anael@barodine.fr>
2020-12-04 17:54:03 +0100

    Ajout de ma signature (Anaël) car le site va être look par des employeurs 👀.

--- -mvc/vue/gabarits/admin/essentiels/footer.php
+++ -mvc/vue/gabarits/admin/essentiels/footer.php
@@ -4,5 +4,6 @@
     <div class="texte-footer">
         <p>Tribu-Terre 2020 | 1A Rue de la Férollerie, 45071, Orléans Cedex 2</p>
         <p><strong>Site Tribu-Terre version <?php echo VERSION_SITE ?></strong></p>
+        <p><small>Développé avec ❤️ par Anaël B.</small></p>
     </div>
 </div>
