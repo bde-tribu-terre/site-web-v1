f07bfb0a8d9592b6246d888c6d98c5dc008d90e8
Anaël iMac <anael@barodine.fr>
2020-12-15 19:21:03 +0100

    Update page de recherche parrainage.

--- -mvc/vue/gabarits/public/parrainageRecherche.php
+++ -mvc/vue/gabarits/public/parrainageRecherche.php
@@ -5,16 +5,18 @@
         <?php if (NB_MEMBRES < 3) { echo '<div class="col-sm-2"></div>'; }?>
         <div class="col-sm-4">
             <div class="well">
-                <p>[<?php echo TYPE0 ?>]</p>
+                <h4 class="pc"><?php echo TYPE0 ?></h4>
                 <p><?php echo NOM0 ?></p>
-                <p><?php echo EMAIL0 ?></p>
+                <p><a href="mailto:<?php echo EMAIL0 ?>"><?php echo EMAIL0 ?></a></p>
+                <p>Cliquez pour envoyer un mail.</p>
             </div>
         </div>
         <div class="col-sm-4">
             <div class="well">
-                <p>[<?php echo TYPE1 ?>]</p>
+                <h4 class="pc"><?php echo TYPE1 ?></h4>
                 <p><?php echo NOM1 ?></p>
-                <p><?php echo EMAIL1 ?></p>
+                <p><a href="mailto:<?php echo EMAIL1 ?>"><?php echo EMAIL1 ?></a></p>
+                <p>Cliquez pour envoyer un mail.</p>
             </div>
         </div>
         <?php
@@ -25,9 +27,10 @@
             echo <<<EOD
 <div class="col-sm-4">
     <div class="well">
-        <p>$type</p>
+        <h4 class="pc">$type</h4>
         <p>$nom</p>
-        <p>$email</p>
+        <p><a href="mailto:$email">$email</a></p>
+        <p>Cliquez pour envoyer un mail.</p>
     </div>
 </div>
 EOD;
@@ -36,44 +39,27 @@ EOD;
         <?php if (NB_MEMBRES < 3) { echo '<div class="col-sm-2"></div>'; }?>
     </div>
     <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+            <div class="well">
+                <h4 class="pc">Groupe</h4>
+                <p>Groupe <?php echo GROUPE ?></p>
+                <p>Ce groupe peut être amené à changer. Nous vous tiendrons informés.</p>
+            </div>
+        </div>
+        <div class="col-sm-3"></div>
+    </div>
+    <div class="row">
         <div class="col-sm-2"></div>
         <div class="col-sm-8">
             <div class="well">
-                <p>[Groupe]</p>
-                <p>Groupe <?php echo GROUPE ?></p>
+                <h4 class="pc">Lien du GForm</h4>
+                <p>À remplir pour que l'on puisse obtenir vos infos pour pouvoir vous assigner à votre groupe.</p>
+                <p><a href="https://forms.gle/yiFgtcV4ynQe8uSV9">https://forms.gle/yiFgtcV4ynQe8uSV9</a></p>
+                <h4 class="pc">Lien du serveur Discord de l'événement</h4>
+                <p><a href="https://discord.gg/nk2ueTtVNc">https://discord.gg/nk2ueTtVNc</a></p>
             </div>
         </div>
         <div class="col-sm-2"></div>
-    </div>
-    <hr>
-    <div class="row">
-        <div class="col-sm-12">
-            <form id="formRechercherMail" method="post" onsubmit="return verifForm(this);">
-                <div class="form-group"> <!-- Email recherché -->
-                    <label for="formRechercherMail_mail">
-                        Saisissez votre e-mail.
-                    </label>
-                    <input
-                            id="formRechercherMail_mail"
-                            name="formRechercherMail_mail"
-                            type="text"
-                            class="form-control"
-                            placeholder="E-mail"
-                            onblur="verifNonVide(this);"
-                            oninput="garderMoins(this, 128);"
-                    >
-                </div>
-                <hr>
-                <div class="form-group"> <!-- Rechercher -->
-                    <input
-                            id="formRechercherMail_rechercher_submit"
-                            name="formRechercherMail_rechercher_submit"
-                            type="submit"
-                            class="btn btn-var btn-block"
-                            value="Rechercher"
-                    >
-                </div>
-            </form>
-        </div>
     </div>
 </div>
