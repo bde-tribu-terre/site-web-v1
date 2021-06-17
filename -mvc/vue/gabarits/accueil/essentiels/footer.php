02e15d7975c01ba9592b30f980ff96c01e2cd7d7
Anaël iMac <anael@barodine.fr>
2021-06-17 18:57:06 +0200

    Changement de la date dans les footers.

--- -mvc/vue/gabarits/accueil/essentiels/footer.php
+++ -mvc/vue/gabarits/accueil/essentiels/footer.php
@@ -7,7 +7,7 @@
     <div class="texte-footer">
         <p>Tribu-Terre est une association étudiante apolitique à but non lucratif régie par la loi du 1er Juillet 1901.</p>
         <p><a href="/plan-du-site/">Plan du site</a> | <a href="/mentions-legales/">Mentions légales</a></p>
-        <p>Tribu-Terre 2020 | 1A Rue de la Férollerie, 45071, Orléans Cedex 2</p>
+        <p>Tribu-Terre 2021 | 1A Rue de la Férollerie, 45071, Orléans Cedex 2</p>
         <p><strong>Site Tribu-Terre version <?php echo VERSION_SITE ?></strong></p>
         <p><small>Développé avec ❤️ par Anaël BARODINE</small></p>
     </div>
--- -mvc/vue/gabarits/admin/essentiels/footer.php
+++ -mvc/vue/gabarits/admin/essentiels/footer.php
@@ -2,7 +2,7 @@
     <p><a href="/">Retourner à l'accueil</a></p>
     <hr>
     <div class="texte-footer">
-        <p>Tribu-Terre 2020 | 1A Rue de la Férollerie, 45071, Orléans Cedex 2</p>
+        <p>Tribu-Terre 2021 | 1A Rue de la Férollerie, 45071, Orléans Cedex 2</p>
         <p><strong>Site Tribu-Terre version <?php echo VERSION_SITE ?></strong></p>
         <p><small>Développé avec ❤️ par Anaël BARODINE</small></p>
     </div>
