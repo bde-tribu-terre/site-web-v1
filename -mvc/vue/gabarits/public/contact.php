2092138cb5574021b6da09a2f2a960e760cea1c9
Anaël iMac <anael@barodine.fr>
2021-07-18 01:15:30 +0200

    feat: supr les adresses mail pour ne laisser que la principale.

--- -mvc/vue/gabarits/public/contact.php
+++ -mvc/vue/gabarits/public/contact.php
@@ -6,9 +6,8 @@
         <div class="col-sm-1"></div>
         <div class="col-sm-10">
             <p class="text-justify retrait">
-                Vous découvrez Tribu-Terre et voulez en savoir plus ? Vous avez simplement une question pour votre
-                bureau ? Vous pouvez contacter chacun de nos pôles sur leurs adresses mails respectives mais aussi notre
-                adresse mail générale.
+                Vous découvrez Tribu-Terre et voulez en savoir plus ? Vous avez simplement une question à poser à votre
+                BDE ? Vous pouvez nous contacter par courriel !
             </p>
         </div>
         <div class="col-sm-1"></div>
@@ -29,104 +28,6 @@
                         id="generale_button"
                         class="btn btn-var"
                         onclick="decoder('generale', 'h4');"
-                >
-                    Cliquer pour décrypter
-                </button>
-            </div>
-        </div>
-        <div class="col-sm-3"></div>
-    </div>
-    <div class="row">
-        <div class="col-sm-6">
-            <div class="well">
-                <h3>Pôle Communication</h3>
-                <hr>
-                <div id="communication_div" style="height: 50px; word-wrap: break-word;">
-                    <p id="communication_base64">
-                        <strong><?php echo base64_encode('tributerre.communication@gmail.com') ?></strong>
-                    </p>
-                </div>
-                <button
-                        id="communication_button"
-                        class="btn btn-var"
-                        onclick="decoder('communication', 'h4');"
-                >
-                    Cliquer pour décrypter
-                </button>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="well">
-                <h3>Pôle Journal</h3>
-                <hr>
-                <div id="journal_div" style="height: 50px; word-wrap: break-word;">
-                    <p id="journal_base64">
-                        <strong><?php echo base64_encode('tributerre.journal@gmail.com') ?></strong>
-                    </p>
-                </div>
-                <button
-                        id="journal_button"
-                        class="btn btn-var"
-                        onclick="decoder('journal', 'h4');"
-                >
-                    Cliquer pour décrypter
-                </button>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-6">
-            <div class="well">
-                <h3>Pôle Culture</h3>
-                <hr>
-                <div id="culture_div" style="height: 50px; word-wrap: break-word;">
-                    <p id="culture_base64">
-                        <strong><?php echo base64_encode('culture@bde-tribu-terre.fr') ?></strong>
-                    </p>
-                </div>
-                <button
-                        id="culture_button"
-                        class="btn btn-var"
-                        onclick="decoder('culture', 'h4');"
-                >
-                    Cliquer pour décrypter
-                </button>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="well">
-                <h3>Pôle Partenariat</h3>
-                <hr>
-                <div id="partenariat_div" style="height: 50px; word-wrap: break-word;">
-                    <p id="partenariat_base64">
-                        <strong><?php echo base64_encode('tributerre.partenariat@gmail.com') ?></strong>
-                    </p>
-                </div>
-                <button
-                        id="partenariat_button"
-                        class="btn btn-var"
-                        onclick="decoder('partenariat', 'h4');"
-                >
-                    Cliquer pour décrypter
-                </button>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-3"></div>
-        <div class="col-sm-6">
-            <div class="well">
-                <h3>Pôle Informatique</h3>
-                <hr>
-                <div id="informatique_div" style="height: 50px; word-wrap: break-word;">
-                    <p id="informatique_base64">
-                        <strong><?php echo base64_encode('informatique@bde-tribu-terre.fr') ?></strong>
-                    </p>
-                </div>
-                <button
-                        id="informatique_button"
-                        class="btn btn-var"
-                        onclick="decoder('informatique', 'h4');"
                 >
                     Cliquer pour décrypter
                 </button>
